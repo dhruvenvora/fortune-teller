8e39fed19ba908fa60f049180fd8ba3553d87597
dhruven <dhruven91@gmail.com>
2016-12-04 14:55:43 -0800

    fetching stock prices for list of companies

--- RetrievePrice.py
+++ RetrievePrice.py
@@ -12,28 +12,29 @@ import datetime as dt
 
 class RetrieveStockPrice(object):
     
-    def __init__(self):
-        self.url = 'http://www.google.com/finance/getprices?i='
-        self.response = None
-        self.data = None
+    url = 'http://www.google.com/finance/getprices?i='
+    
+    def __init__(self, period, window):
+        self.period = period        
+        self.window = window
+        self.url += str(self.period) + '&p=' + str(self.window)
         
-    def get_google_data(self, symbol, period, window):
-        self.url += str(period) + '&p=' + str(window)
-        self.url += 'd&f=d,o,h,l,c&df=cpct&q=' + symbol
-        self.response = urllib2.urlopen(self.url)
-        self.data = self.response.read().split('\n')
+    def getStockPrices(self, ticker):
+        query = self.url + 'd&f=d,o,h,l,c&df=cpct&q=' + ticker
+        response = urllib2.urlopen(query)
+        data = response.read().split('\n')
         parsed_data = []
         anchor_stamp = ''
-        end = len(self.data)
+        end = len(data)
         for i in range(7, end):
-            cdata = self.data[i].split(',')
+            cdata = data[i].split(',')
             if 'a' in cdata[0]:
                 anchor_stamp = cdata[0].replace('a', '')
                 cts = int(anchor_stamp)
             else:
                 try:
                     coffset = int(cdata[0])
-                    cts = int(anchor_stamp) + (coffset * period)
+                    cts = int(anchor_stamp) + (coffset * self.period)
                     parsed_data.append((dt.datetime.fromtimestamp(float(cts)), \
                            float(cdata[1]), float(cdata[2]), float(cdata[3]), \
                            float(cdata[4])))
@@ -44,3 +45,13 @@ class RetrieveStockPrice(object):
         df.index = df.ts
         del df['ts']
         return df
+        
+    def getStockPricesForCompanies(self, tickers):
+        
+        stockPriceVsTicker = {}
+        
+        for ticker in tickers:
+            df = self.getStockPrices(ticker)
+            stockPriceVsTicker[ticker] = df
+            
+        return stockPriceVsTicker
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import json_parser as jpar
+import RetrievePrice as rp
 reload(jpar)
 
 def main():
-    parse_data = jpar.ParseData()
-    parse_data.extractArticlesFromJSON('Data/amazon.json')
+    #parse_data = jpar.ParseData()
+    #parse_data.extractArticlesFromJSON('Data/amazon.json')
     
+    rs = rp.RetrieveStockPrice(600, 2)
+    print rs.getStockPricesForCompanies(['amzn', 'infy'])
+
 if __name__ == "__main__":
     main()
