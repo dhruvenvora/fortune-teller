234a4efd0220abfe18f6ec2dfe949236c4f4a444
dhruven <dhruven91@gmail.com>
2016-12-03 17:08:24 -0800

    Added classes Article and StockPrice. Added utility methods.

---
+++ objects.py
@@ -0,0 +1,31 @@
+#Objects
+
+
+# This class contains the details of an article with publication time. The class also maps to
+# the list of stock prices.
+class Article:
+    
+    def __init__(self, title, timestamp, sentimentScore):
+        self.title = title
+        self.timestamp = timestamp
+        self.sentimentScore = sentimentScore
+        self.stockPrices = []
+
+    def addStock(self, stockPrice):
+        self.stocksPrices.append(stockPrice)
+
+
+# This class stores the price imformarion of a stock at given time.
+class StockPrice:
+
+    def __init__(self, entity, price, timestamp):
+        self.entity = entity
+        self.price = price
+        self.timestamp = timestamp
+        self.article = None
+    
+    def mapArticle(self, article):
+        self.article = article
+
+
+
