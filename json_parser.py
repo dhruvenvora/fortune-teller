2e27fe48719ff08feb78f7808ff7e970a46cde44
Vikash Raja Samuel Selvin <selvin.samuel2@gmail.com>
2016-12-06 23:12:27 -0800

    Added Timestamp logic

--- json_parser.py
+++ json_parser.py
@@ -4,6 +4,7 @@ import json
 import objects as obj
 import constants as ct
 import RetrievePrice as rp
+import datetime as dt
 
 reload(obj)
 reload(ct)
@@ -21,12 +22,19 @@ class ParseData(object):
         docs = data['result']['docs']
 
         for doc in docs:
-            timestamp = doc['timestamp']
-            title = doc['source']['enriched']['url']['title']
-            sentimentScore = doc['source']['enriched']['url']['docSentiment']['score']
-            date = doc['source']['enriched']['url']['publicationDate']['date']
-            article = obj.Article(title,timestamp,sentimentScore,date,ct._TICKERS[company])
-            articles.append(article)
+            #timestamp = doc['timestamp']
+            # If the publication date is 2016 then process it, else skip
+            if doc['source']['enriched']['url']['publicationDate']['date'][:4] == '2016':
+                year = int(doc['source']['enriched']['url']['publicationDate']['date'][:4] )
+                month = int(doc['source']['enriched']['url']['publicationDate']['date'][4:6] )
+                day = int(doc['source']['enriched']['url']['publicationDate']['date'][6:8] )
+                timestamp = int((dt.datetime(year,month,day,0,0) - dt.datetime(1970,1,1)).total_seconds())
+
+                title = doc['source']['enriched']['url']['title']
+                sentimentScore = doc['source']['enriched']['url']['docSentiment']['score']
+                date = doc['source']['enriched']['url']['publicationDate']['date']
+                article = obj.Article(title,timestamp,sentimentScore,date,ct._TICKERS[company])
+                articles.append(article)
         return articles
 
 
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ reload(jpar)
 
 def main():
     #companies = ['amazon','Cisco','IBM','Infosys','symantec','Visa']
-    companies = ['amazon']
+    companies = ['Cisco']
     company_articles = {}          #Stores company and its corresponding article
     company_timestamp = {}         #Stores company and its corresponding timestamp
     parse_data = jpar.ParseData()
@@ -38,17 +38,23 @@ def main():
     relevant_sp = {}
     for org in company_timestamp.keys():
         for ts in company_timestamp[org]:
+            relevant_sp[org] = {} 
+    temp_ts = []
+    for org in company_timestamp.keys():
+        for ts in company_timestamp[org]:
             #ts_converted = dt.datetime.fromtimestamp(ts)
             #ts_converted_int = dt.datetime.utcfromtimestamp(1477315800) # Need this value
             #ts_converted = dt.datetime.utcfromtimestamp(1477315800).strftime('%m-%d-%Y %H:%M:%S')
-            ts_converted = dt.datetime.utcfromtimestamp(1477315800)
-            oneDayBeforeTS = (ts_converted + dt.timedelta(hours=16)).strftime('%Y-%m-%d %H:%M:%S')
-            oneDayAfterTS = (ts_converted + dt.timedelta(hours=-16)).strftime('%Y-%m-%d %H:%M:%S')
+            ts_converted = dt.datetime.utcfromtimestamp(ts)
+            oneDayBeforeTS = (ts_converted + dt.timedelta(hours=-16)).strftime('%Y-%m-%d %H:%M:%S')
+            oneDayAfterTS = (ts_converted + dt.timedelta(hours=16)).strftime('%Y-%m-%d %H:%M:%S')
             ts_converted = ts_converted.strftime('%Y-%m-%d %H:%M:%S')
+            temp_ts.append(ts_converted)
             # Filter the stock prices for 16 hours before and after the news article
-            print "Time Stamp : %s\n1 Day Before : %s\n1 Day After : %s\n" % (ts,oneDayBeforeTS,oneDayAfterTS)
-            relevant_sp[org] = res[ct._TICKERS[org]].loc[(res['CSCO']['ts'] >= oneDayBeforeTS) & (res['CSCO']['ts'] <= oneDayAfterTS)]
+            #print "Time Stamp : %s\n1 Day Before : %s\n1 Day After : %s\n" % (ts,oneDayBeforeTS,oneDayAfterTS)
+            relevant_sp[org][ts_converted] = res[ct._TICKERS[org]].loc[(res[ct._TICKERS[org]]['ts'] >= oneDayBeforeTS) & (res[ct._TICKERS[org]]['ts'] <= oneDayAfterTS)]
     print "Relevant Stock Price : %s" % relevant_sp
+    print "No of distinct timestamps : ", relevant_sp[org].keys()#, len(company_timestamp[org]), company_timestamp[org], temp_ts
 
 
 if __name__ == "__main__":
